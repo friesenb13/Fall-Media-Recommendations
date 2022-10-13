1d8e125394ada5d912e7e20c3e396d0cc104dc88
Becky <77301055+friesenb13@users.noreply.github.com>
2022-10-13 16:56:29 -0600

    trying photo browser

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,33 @@ var app = new Framework7({
         url: 'pages/page3.html',
       },
     ],
-
+    
+    
   });
 var mainView = app.views.create('.view-main')
+
+var swiper = app.swiper.get('.swiper');
+
+swiper.slideNext();
+
+var photos = [
+  'image1.jpg',
+  'image2.jpg',
+  'image3.jpg',
+  'image4.jpg',
+];
+
+//If we need to use caption for some of photos then each photo should be presented as object:
+var photos = [
+  {
+      url: '/img/gilmore1.jpg',
+  },
+  {
+      url: 'image2.jpg',
+  },
+  {
+      url: 'image3.jpg',
+  },
+  // This one will be also without caption
+  'image4.jpg'
+];

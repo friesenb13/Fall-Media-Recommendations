6f8a4c3d46b8a2bed28afcf110c51ffd2572b7c5
Becky <77301055+friesenb13@users.noreply.github.com>
2022-10-13 17:39:59 -0600

    swipe update

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,6 @@
 var app = new Framework7({
     // App root element
+    
     el: '#app',
     // ... other parameters
     routes: [
@@ -21,28 +22,6 @@ var app = new Framework7({
   });
 var mainView = app.views.create('.view-main')
 
-var swiper = app.swiper.get('.swiper');
+// var swiper = app.swiper.get('.swiper');
 
-swiper.slideNext();
-
-var photos = [
-  'image1.jpg',
-  'image2.jpg',
-  'image3.jpg',
-  'image4.jpg',
-];
-
-//If we need to use caption for some of photos then each photo should be presented as object:
-var photos = [
-  {
-      url: '/img/gilmore1.jpg',
-  },
-  {
-      url: 'image2.jpg',
-  },
-  {
-      url: 'image3.jpg',
-  },
-  // This one will be also without caption
-  'image4.jpg'
-];
+//   swiper.slideNext();
